22cef0149234902ca3c9ef9b9f5185e3b23d571d
luukaszp <luukaszpoterala@wp.pl>
2021-08-28 18:18:17 +0200

    footer view

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,70 +1,98 @@
 <footer class="bg-light text-center text-white">
-    <!-- Grid container -->
     <div class="container p-4 pb-0">
-      <!-- Section: Social media -->
-      <section class="mb-4">
-        <!-- Youtube -->
-        <a
+        <hr class="my-4" style="color: black">
+        <div class="row text-black py-5">
+            <div class="col-sm">
+                <div class="list-group list-group-flush">
+                    <p>POMOC</p>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Zwroty i reklamacje</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Pytania i odpowiedzi</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Regulamin</a>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="list-group list-group-flush">
+                    <p>MOJE KONTO</p>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Twoje zamówienia</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Ustawienia konta</a>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="list-group list-group-flush">
+                    <p>PŁATNOŚCI I DOSTAWA</p>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Formy płatności</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Czas i koszty dostawy</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Czas realizacji zamówienia</a>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="list-group list-group-flush">
+                    <p>INFORMACJE</p>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Polityka prywatności</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Jak kupować?</a>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="list-group list-group-flush">
+                    <p>O NAS</p>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">Kontakt i dane firmy</a>
+                    <a href="/" class="list-group-item list-group-item-action list-group-item-secondary">O firmie</a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4" style="color: black">
+        <section class="mb-4 pt-5">
+            <a
+                class="btn btn-primary btn-floating m-1"
+                style="background-color: #FF0000;"
+                href="https://www.youtube.com/"
+                role="button"
+                ><i class="fab fa-youtube"></i
+                ></a>
+
+            <a
             class="btn btn-primary btn-floating m-1"
-            style="background-color: #FF0000;"
-            href="https://www.youtube.com/"
+            style="background-color: #3b5998;"
+            href="https://www.facebook.com/"
             role="button"
-            ><i class="fab fa-youtube"></i
+            ><i class="fab fa-facebook-f"></i
             ></a>
 
-        <!-- Facebook -->
-        <a
-           class="btn btn-primary btn-floating m-1"
-           style="background-color: #3b5998;"
-           href="https://www.facebook.com/"
-           role="button"
-           ><i class="fab fa-facebook-f"></i
-          ></a>
+            <a
+            class="btn btn-primary btn-floating m-1"
+            style="background-color: #55acee;"
+            href="https://twitter.com/"
+            role="button"
+            ><i class="fab fa-twitter"></i
+            ></a>
 
-        <!-- Twitter -->
-        <a
-           class="btn btn-primary btn-floating m-1"
-           style="background-color: #55acee;"
-           href="https://twitter.com/"
-           role="button"
-           ><i class="fab fa-twitter"></i
-          ></a>
+            <a
+            class="btn btn-primary btn-floating m-1"
+            style="background-color: #dd4b39;"
+            href="https://www.google.com/"
+            role="button"
+            ><i class="fab fa-google"></i
+            ></a>
 
-        <!-- Google -->
-        <a
-           class="btn btn-primary btn-floating m-1"
-           style="background-color: #dd4b39;"
-           href="https://www.google.com/"
-           role="button"
-           ><i class="fab fa-google"></i
-          ></a>
+            <a
+            class="btn btn-primary btn-floating m-1"
+            style="background-color: #ac2bac;"
+            href="https://www.instagram.com/"
+            role="button"
+            ><i class="fab fa-instagram"></i
+            ></a>
 
-        <!-- Instagram -->
-        <a
-           class="btn btn-primary btn-floating m-1"
-           style="background-color: #ac2bac;"
-           href="https://www.instagram.com/"
-           role="button"
-           ><i class="fab fa-instagram"></i
-          ></a>
-
-        <!-- Pinterest -->
-        <a
-           class="btn btn-primary btn-floating m-1"
-           style="background-color: #E60023;"
-           href="https://pl.pinterest.com/"
-           role="button"
-           ><i class="fab fa-pinterest"></i
-          ></a>
-      </section>
-      <!-- Section: Social media -->
-    </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
+            <a
+            class="btn btn-primary btn-floating m-1"
+            style="background-color: #E60023;"
+            href="https://pl.pinterest.com/"
+            role="button"
+            ><i class="fab fa-pinterest"></i
+            ></a>
+        </section>
+    </div>>
     <div class="text-center p-3 bg-dark" style="background-color: rgba(0, 0, 0, 0.2);">
-      Copyrights © 2021 -
-      <a class="text-white" href="https://sklep-meblowy.pl/">Sklep Meblowy</a>
+        Copyrights © 2021 -
+        <a class="text-white" href="https://sklep-meblowy.pl/">Sklep Meblowy</a>
     </div>
-    <!-- Copyright -->
   </footer>
