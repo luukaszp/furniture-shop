a570ff5acc0fd8efcfe0e42c1a234cd65f206add
luukaszp <luukaszpoterala@wp.pl>
2021-10-14 12:21:09 +0200

    file session

--- config/database.php
+++ config/database.php
@@ -118,13 +118,30 @@ return [
     */
 
     'redis' => [
-        'client' => env('REDIS_CLIENT', 'predis'),
+
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
         'default' => [
-            'url' => 'tcp://127.0.0.1:6379?database=0',
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
         ],
+
         'cache' => [
-            'url' => 'tls://user:password@127.0.0.1:6380?database=1',
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
         ],
+
     ],
 
 ];
